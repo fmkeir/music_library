e5e6829ad98ca6200c4f44f4163648cf9f9abc8e
FKeir <fmkeir@gmail.com>
2020-01-29 14:59:03 +0000

    Create album with method

---
+++ models/album.rb
@@ -0,0 +1,26 @@
+require('pg')
+require_relative("../db/sql_runner")
+
+class Album
+
+  attr_reader :name, :genre, :artist_id
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+    @genre = options["genre"]
+    @artist_id = options["artist_id"].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO albums
+    (name, genre, artist_id),
+    VALUES
+    ($1, $2, $3)
+    RETURNING id"
+    values = [@name, @genre, @artist_id]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]["id"].to_i()
+  end
+
+end
